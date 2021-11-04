a05176ebbe8705f5d17b3575c0ec32ba621d6e5d
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-11-04 16:32:24 +0900

    #6.3 Testing Setup

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import auth from "@react-native-firebase/auth";
+import { useEffect } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  useEffect(() => {
+    console.log(auth().currentUser);
+  });
+  return null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
